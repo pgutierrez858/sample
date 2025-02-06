c5093685707a5f4c9c6ca67eed65d627fcf3830a
Pablo Gutiérrez <44195639+pgutierrez858@users.noreply.github.com>
2025-02-06 09:57:05 +0100

    Update operaciones.js

--- operaciones.js
+++ operaciones.js
@@ -3,3 +3,5 @@ export const sumar = (a, b) => a + b; // esto era sumar
 export const multiplicar = (a, b) => {
   return a * b;
 }; // esto era multiplicar
+
+export const restar = (a, b) => a - b; // esto es restar
